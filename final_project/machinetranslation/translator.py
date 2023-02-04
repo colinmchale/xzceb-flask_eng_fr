9aee5e3006610bba681cc22ae62f686597215bca
Colin McHale <93059063+colinmchale@users.noreply.github.com>
2023-02-03 19:42:02 -0600

    added function to start instance, added e2f and f2e translator functions

--- final_project/machinetranslation/translator.py
+++ final_project/machinetranslation/translator.py
@@ -8,3 +8,37 @@ load_dotenv()
 
 apikey = os.environ['apikey']
 url = os.environ['url']
+
+def translator_instance():
+    """
+    creates instance
+    """
+    authenticator = IAMAuthenticator(apikey)
+    language_translator = LanguageTranslatorV3(
+        version='2018-05-01',
+        authenticator=authenticator
+    )
+    language_translator.set_service_url(url)
+
+    return language_translator
+
+def english_to_french(english_text):
+
+    translation = language_translator.translate(
+        text = english_text,
+        model_id ='en-fr').get_result()
+    french_text = translation.get('translations')[0].get('translation')
+
+    return french_text
+
+def french_to_english(french_text):
+
+    translation = language_translator.translate(
+        text = french_text,
+        model_id ='fr-en').get_result()
+    english_text = translation.get('translations')[0].get('translation')
+    
+    return english_text
+
+print(english_to_french("Hello"))
+print(french_to_english("Bonjour"))
