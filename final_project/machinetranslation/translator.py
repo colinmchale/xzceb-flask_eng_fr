8ebcad4d58f21a7b0a7f1aeb314f39499e046e35
Colin McHale <93059063+colinmchale@users.noreply.github.com>
2023-02-03 21:31:44 -0600

    created and ran tests, cleaned errors in code after pylint, created new folder with tests inside

--- final_project/machinetranslation/translator.py
+++ final_project/machinetranslation/translator.py
@@ -1,7 +1,8 @@
 import json
+import os
+
 from ibm_watson import LanguageTranslatorV3
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
-import os
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -9,18 +10,12 @@ load_dotenv()
 apikey = os.environ['apikey']
 url = os.environ['url']
 
-def translator_instance():
-    """
-    creates instance
-    """
-    authenticator = IAMAuthenticator(apikey)
-    language_translator = LanguageTranslatorV3(
-        version='2018-05-01',
-        authenticator=authenticator
-    )
-    language_translator.set_service_url(url)
-
-    return language_translator
+authenticator = IAMAuthenticator(apikey)
+language_translator = LanguageTranslatorV3(
+    version = '2018-05-01',
+    authenticator = authenticator
+)
+language_translator.set_service_url(url)
 
 def english_to_french(english_text):
 
@@ -37,7 +32,7 @@ def french_to_english(french_text):
         text = french_text,
         model_id ='fr-en').get_result()
     english_text = translation.get('translations')[0].get('translation')
-    
+
     return english_text
 
 print(english_to_french("Hello"))
