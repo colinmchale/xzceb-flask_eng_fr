5222289c8b8bc10a97e05c84b3d787b7c21bffec
Colin McHale <93059063+colinmchale@users.noreply.github.com>
2023-02-03 20:00:02 -0600

    added tests for e2f and f2e functions

---
+++ final_project/machinetranslation/tests.py
@@ -0,0 +1,18 @@
+import unittest
+
+from translator import english_too_french, french_to_english
+
+
+class TestEnglishToFrench(unittest.TestCase): 
+    def test1(self): 
+        self.assertNotEqual(english_to_french(0),0) 
+    def test2(self): 
+        self.assertEqual(english_to_french("Hello"), "Bonjour") 
+
+class TestFrenchToEnglish(unittest.TestCase): 
+    def test1(self): 
+        self.assertNotEqual(french_to_english(0),0)
+    def test2(self): 
+        self.assertEqual(french_to_english("Bonjour"), "Hello") 
+
+unittest.main()
